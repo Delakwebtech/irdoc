fb96dd88e9b25014183bc5ee697d256e487b5699
Delakwebtech <79271954+Delakwebtech@users.noreply.github.com>
2025-06-03 08:38:00 +0100

    feat: add data files for colleges of education, health sciences, polytechnics, and universities

---
+++ _data/healthSciences.js
@@ -0,0 +1,17 @@
+const healthSciences = [
+    "Ahmadu Bello University Teaching Hospital P.M.B. , Shika, Zaria, Kaduna State",
+    "Aminu Kano Teaching Hospital, School of Health Information Management, Kano, Kano State",
+    "School of Health Information Management, Lagos University Teaching Hospital, Idi-Araba, Lagos State",
+    "School of Health Information Management, Obafemi Awolowo University Teaching Hospital Complex, Ile-Ife, Osun State",
+    "Shehu Idris College of Health Sciences and Technology, Makarfi, Kaduna State",
+    "School of Health Information Management, University of Benin Teaching Hospital, Benin City, Edo State",
+    "School of Health Information Management, University College Hospital, Ibadan, Oyo State",
+    "School of Health Information Management, University of Maiduguri Teaching Hospital, Maiduguri, Borno State",
+    "University of Ilorin Teaching Hospital, School of Health Information Management, Ilorin, Kwara State",
+    "School of Health Information Management, University of Uyo Teaching Hospital, Uyo, Akwa Ibom State",
+    "University of Calabar Teaching Hospital, School of Health Information Management, Calabar",
+    "University of Port Harcourt Teaching Hospital, Port Harcourt, Rivers State",
+    "School of Health Information Management, Nnamdi Azikiwe University Teaching Hospital, Nnewi, Anambra State",
+];
+
+module.exports = healthSciences;
